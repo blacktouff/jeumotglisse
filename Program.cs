445b49ac8afd9c9c215c871d225cbd200d81d01a
thoma <thoma@DESKTOP-EHMOSER>
2023-11-30 20:20:29 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using jeumotglisse.objets;
+
+Console.WriteLine("Hello, World!");
+Class1 t = new Class1("lala");
+
+Console.WriteLine(t.toString());
+
+Console.Write("Hey jeune lady comment tu t'intitule ? ");
+string userInput = Console.ReadLine();
+Console.WriteLine("Wesh " + userInput);
